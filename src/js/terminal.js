935afe4031d4cb6f7b5da7d57554eb511117327e
samuel <samuelorlato@gmail.com>
2022-04-21 13:53:13 -0300

    Added commands to customize colors and save it with cookies, added duckduckgo (d) command

--- src/js/terminal.js
+++ src/js/terminal.js
@@ -1,47 +1,73 @@
 // Terminal config
 config = {
-    shellPrompt: "$ "
+	shellPrompt: "$ ",
 }
 
-document.getElementById("input_title").innerText = config.shellPrompt;
-document.getElementById('input_source').addEventListener('keyup', submit_command);
-document.getElementById('input_source').addEventListener('keyup', getLastCommand);
+document.getElementById("input_title").innerText = config.shellPrompt
+document
+	.getElementById("input_source")
+	.addEventListener("keyup", submit_command)
+document
+	.getElementById("input_source")
+	.addEventListener("keyup", getLastCommand)
 
-var current_block;
+var current_block
 
 function new_block() {
-    current_block = document.createElement("div");
-    current_block.classList.add("log");
-    document.getElementById('wrapper').appendChild(current_block);
+	current_block = document.createElement("div")
+	current_block.classList.add("log")
+	document.getElementById("wrapper").appendChild(current_block)
 }
 
 function block_log(message) {
-    current_block.innerHTML = "<p>" + message + "</p>";
-    new_block();
+	current_block.innerHTML = "<p>" + message + "</p>"
+	new_block()
 }
 
 function submit_command() {
-    if (!(event.keyCode === 13)) return;
-    var input = document.getElementById("input_source").value;
-    document.getElementById("input_source").value = "";
+	if (!(event.keyCode === 13)) return
+	var input = document.getElementById("input_source").value
+	document.getElementById("input_source").value = ""
 
-    new_block();
+	new_block()
 
-    command = input.split(" ")[0];
-    args = input.replace(command, "")
+	command = input.split(" ")[0]
+	args = input.replace(command, "")
 
-    if (typeof window[command] === "function") {
-        block_log(config.shellPrompt + command + " " + args);
-        window[command](args);
-        lastCommand = command + args;
-    } else if (command != "") {
-        block_log("command not found : " + command);
-    }
+	if (typeof window[command] === "function") {
+		block_log(config.shellPrompt + command + " " + args)
+		window[command](args)
+		lastCommand = command + args
+	} else if (command != "") {
+		block_log("command not found : " + command)
+	}
 }
 
 function getLastCommand() {
-    if (!(event.keyCode === 38)) return;
-    if (lastCommand) {
-        document.getElementById("input_source").value = lastCommand;
-    }
+	if (!(event.keyCode === 38)) return
+	if (lastCommand) {
+		document.getElementById("input_source").value = lastCommand
+	}
 }
+
+cookies = document.cookie.split(";")
+cookies.forEach((cookie) => {
+	if (cookie.includes("promptcolor")) {
+		document.documentElement.style.setProperty(
+			"--prompt-bg-color",
+			cookie.slice(cookie.indexOf("=") + 1)
+		)
+	}
+	if (cookie.includes("textcolor")) {
+		document.documentElement.style.setProperty(
+			"--text-color",
+			cookie.slice(cookie.indexOf("=") + 1)
+		)
+	}
+	if (cookie.includes("bgcolor")) {
+		document.documentElement.style.setProperty(
+			"--main-bg-color",
+			cookie.slice(cookie.indexOf("=") + 1)
+		)
+	}
+})
